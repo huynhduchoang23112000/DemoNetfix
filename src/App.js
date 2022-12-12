5a1759262e85f2f284250c358c3075904d259d7c
Duc Hoang <huynhduchoang2000@gmail.com>
2022-12-12 21:27:03 +0700

    CodeDetail

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import './components/Navbar/Navbar';
 import Navbar from './components/Navbar/Navbar';
 import Intro from './components/Intro/Intro';
 import Contents from './components/Contents/Contents';
+import MoviesDetail from './components/MoviesDetail/MoviesDetail';
 import { useSelector } from 'react-redux';
 function App() {
+  const {MovieDetail} = useSelector(state => state.infoMovies)
   return (
     <div className="App">
       <Navbar />
       <Intro />
       <Contents />
+      <MoviesDetail movie={MovieDetail} showModal={MovieDetail ? true : false}/>
     </div>
   );
 }
